7c78a30d0025d39c1e7ec022a8564c8799afa04e
LehuanPi <80317951+LehuanPi@users.noreply.github.com>
2021-03-09 17:36:19 +0800

    Add files via upload

---
+++ Jason衣服商城系统.py
@@ -0,0 +1,38 @@
+id1 = 1
+name1 = "大白褂"
+price1 = 100
+quanlity1 = "s"
+type1 = "上衣"
+num1 = 100
+
+id2 = 2
+name2 = "大裤衩"
+price2 = 75
+quanlity2 = "s"
+type2 = "裤子"
+num2 = 100
+
+id3 = 3
+name3 = "小白褂"
+price3 = 175
+quanlity3 = "s"
+type3 = "裤子"
+num3 = 100
+
+id4 = 4
+name4 = "小裤衩"
+price4 = 275
+quanlity4 = "s"
+type4 = "裤子"
+num4 = 100
+
+print("-----------------------------")
+print("   欢迎来到Jason衣服商城系统     ")
+print("-----------------------------")
+print("编号  名称  价格  品质  类型  销量")
+print(id1,"",name1,"",price1,"",quanlity1,"",type1,"",num1)
+print(id2,"",name2,"",price2,"",quanlity2,"",type2,"",num2)
+print(id3,"",name3,"",price3,"",quanlity3,"",type3,"",num3)
+print(id4,"",name4,"",price4,"",quanlity4,"",type4,"",num4)
+print("-----------------------------")
+print("总金额",(price1 * num1 + price2 * num2 + price3 * num3 + price4 * num4),"￥")
